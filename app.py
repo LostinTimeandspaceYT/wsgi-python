19068855b7b1184de30bbaad20b467236d383356
Nathan Winslow <110609695+Nate-NAI@users.noreply.github.com>
2023-02-22 08:47:31 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+from parse import parse
+from webob import Request, Response
+
+
+class API:
+
+    def __init__(self):
+        self.routes = {}
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        response = self.handle_request(request)
+        return response(environ, start_response)
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found."
+
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            parse_result = parse(path, request_path)
+            if parse_result is not None:
+                return handler, parse_result.named
+        return None, None
+
+    def handle_request(self, request):
+        response = Response()
+
+        handler, kwargs = self.find_handler(request_path=request.path)
+
+        if handler is not None:
+            handler(request, response, **kwargs)
+        else:
+            self.default_response(response)
+
+        return response
+
+
+application = API()
+
+
+@application.route("/app")
+def home(request, response):
+    response.text = "Hello from the APP page"
+
+
+@application.route("/app/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
+
+
+@application.route("/app/about")
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
+
+
+@application.route("/app/hello/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello, {name}"
