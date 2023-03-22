3e0fd471038a6b9126546f5e44579d26073c0bde
Nathan Winslow <110609695+Nate-NAI@users.noreply.github.com>
2023-03-22 13:02:45 -0400

    Added Queries

--- app.py
+++ app.py
@@ -1,6 +1,107 @@
 from parse import parse
 from webob import Request, Response
+from mysql.connector import (connection)
+from mysql.connector import errorcode
+import threading
+import pprint
+import time
+import os
+import json
 
+class create_dict(dict): 
+  
+    # __init__ function 
+    def __init__(self): 
+        self = dict() 
+          
+    # Function to add key:value 
+    def add(self, key, value): 
+        self[key] = value
+
+class LoggingInstance:
+    def __init__(self, start_response, oheaders, ocontent):
+        self.__start_response = start_response
+        self.__oheaders = oheaders
+        self.__ocontent = ocontent
+
+    def __call__(self, status, headers, *args):
+        pprint.pprint((status, headers)+args, stream=self.__oheaders)
+        self.__oheaders.close()
+
+        self.__write = self.__start_response(status, headers, *args)
+        return self.write
+
+    def __iter__(self):
+        return self
+
+    def write(self, data):
+        self.__ocontent.write(data)
+        self.__ocontent.flush()
+        return self.__write(data)
+
+    def next(self):
+        data = self.__iterable.next()
+        self.__ocontent.write(data)
+        self.__ocontent.flush()
+        return data
+
+    def close(self):
+        if hasattr(self.__iterable, 'close'):
+            self.__iterable.close()
+        self.__ocontent.close()
+
+    def link(self, iterable):
+        self.__iterable = iter(iterable)
+
+class LoggingMiddleware:
+
+    def __init__(self, application, savedir):
+        self.__application = application
+        self.__savedir = savedir
+        self.__lock = threading.Lock()
+        self.__pid = os.getpid()
+        self.__count = 0
+
+    def __call__(self, environ, start_response):
+        self.__lock.acquire()
+        self.__count += 1
+        count = self.__count
+        self.__lock.release()
+
+        key = "%s-%s-%s" % (time.time(), self.__pid, count)
+
+        iheaders = os.path.join(self.__savedir, key + ".iheaders")
+        iheaders_fp = open(iheaders, 'w')
+
+        icontent = os.path.join(self.__savedir, key + ".icontent")
+        icontent_fp = open(icontent, 'w+b')
+
+        oheaders = os.path.join(self.__savedir, key + ".oheaders")
+        oheaders_fp = open(oheaders, 'w')
+
+        ocontent = os.path.join(self.__savedir, key + ".ocontent")
+        ocontent_fp = open(ocontent, 'w+b')
+
+        errors = environ['wsgi.errors']
+        pprint.pprint(environ, stream=iheaders_fp)
+        iheaders_fp.close()
+
+        length = int(environ.get('CONTENT_LENGTH', '0'))
+        input = environ['wsgi.input']
+        while length != 0:
+            data = input.read(min(4096, length))
+            if data:
+                icontent_fp.write(data)
+                length -= len(data)
+            else:
+                length = 0
+        icontent_fp.flush()
+        icontent_fp.seek(0, os.SEEK_SET)
+        environ['wsgi.input'] = icontent_fp
+
+        iterable = LoggingInstance(start_response, oheaders_fp, ocontent_fp)
+        iterable.link(self.__application(environ, iterable))
+        return iterable
 
 class API:
 
@@ -40,27 +141,79 @@ class API:
         else:
             self.default_response(response)
 
+
         return response
 
 
 application = API()
-
-
-@application.route("/app")
-def home(request, response):
-    response.text = "Hello from the APP page"
-
-
-@application.route("/app/home")
-def home(request, response):
-    response.text = "Hello from the HOME page"
-
-
-@application.route("/app/about")
-def about(request, response):
-    response.text = "Hello from the ABOUT page"
+config = {
+    'user': 'root',
+    'password': 'nhti',
+    'host': '127.0.0.1',
+    'port': '3306',
+    'database': 'movies',
+    'raise_on_warnings': True
+}
 
 
 @application.route("/app/hello/{name}")
 def greeting(request, response, name):
     response.text = f"Hello, {name}"
+
+
+@application.route("/app/theaters")
+def theaters(request, response):
+    cnx = connection.MySQLConnection(**config)
+    cursor = cnx.cursor()
+    query = "SELECT * from Theater"
+    cursor.execute(query)
+    theaters = []
+    for t in cursor:
+        theaters.append({"theater_id": t[0], "name":t[1], "address":t[2], "city":t[3], "state":t[4],
+                        "zip":t[5], "phone":t[6], "website":t[7]})
+    
+    json_str = json.dumps(theaters, indent=2, sort_keys=True, default=str)
+    print(json_str.encode())
+    response.text = json_str
+    cnx.close()
+
+
+@application.route("/app/movies")
+def movies(request, response):
+
+    cnx = connection.MySQLConnection(**config)
+    cursor = cnx.cursor()
+    query = "SELECT * FROM Movie"
+    cursor.execute(query)
+    movies = []
+
+    for title in cursor:
+        movies.append({"m_id": title[0], "m_title": title[1], "genre": title[2], "minutes": title[3],
+                      "release_date": title[4], "budget": title[5], "r_id": title[6], "studio_id": title[7]})
+        
+    json_str = json.dumps(movies, indent=2, sort_keys=True, default=str)
+    print(json_str.encode())
+    response.text = json_str  
+    cnx.close()
+
+
+@application.route("/app/movies/{id}")
+def get_movie(request, response, id):
+    print(f" id = {id}")
+
+    cnx = connection.MySQLConnection(**config)
+    cursor = cnx.cursor()
+    query = f"SELECT * FROM Movie WHERE m_id={id}"
+
+    cursor.execute(query)
+    movie = []
+    for title in cursor:
+        movie.append({"m_id": title[0], "m_title": title[1], "genre": title[2], "minutes": title[3],
+                      "release_date": title[4], "budget": title[5]})
+    
+    json_str = json.dumps(movie, indent=2, sort_keys=True, default=str)
+    print(json_str.encode())
+    response.text = json_str
+    cnx.close()
+
+
